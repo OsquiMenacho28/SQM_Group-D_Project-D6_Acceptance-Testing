769f9f21d2dcb6c48622d5cc849187efc86d54de
JesusRodrigoV <jaicelvelasco105@gmail.com>
2026-06-08 17:25:34 -0400

    fix(luzticona): change BeforeTest/AfterTest to BeforeClass/AfterClass

--- src/test/java/edu/bo/ucb/luzticona/BaseTest.java
+++ src/test/java/edu/bo/ucb/luzticona/BaseTest.java
@@ -5,8 +5,8 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;   
-import org.testng.annotations.BeforeTest;  
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -20,7 +20,7 @@ public abstract class BaseTest {
     protected static String createdUsuarioEmail;
     protected static String createdUsuarioUsername;
 
-    @BeforeTest   // ← Ahora es @BeforeTest
+    @BeforeClass
     public void setUp() throws Exception {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
@@ -63,7 +63,7 @@ public abstract class BaseTest {
         }
     }
 
-    @AfterTest   // ← Ahora es @AfterTest
+    @AfterClass
     public void tearDown() throws Exception {
         if (driver != null) {
             driver.quit();
